59fb067a21be090fe3863a8208a8e30be3218f55
Marek <marekslad@hotmail.com>
2017-03-20 23:13:35 +0100

    170320_2315

--- index.php
+++ index.php
@@ -186,7 +186,6 @@ mysqli_query($conn, 'SET NAMES `utf8` COLLATE `utf8_general_ci`');
                           <?php
                               }
                           }
-                          $conn->close();
                           ?>
                         </div>
                         <div class="col col-2"></div>
@@ -290,53 +289,53 @@ mysqli_query($conn, 'SET NAMES `utf8` COLLATE `utf8_general_ci`');
                 <div class="col">
                     <h2>Muzyka i Wideo</h2>
                     <div class="row">
-                        <div class="col col-2"></div>
-                        <div class="col col-8 video-gallery">
-                            <div class="video">
-                                <span class="video-title">Majestic - Boberek # Woodstock 2016 Live</span>
-                                <div class="youtube" data-embed="pFoSLLoRYMQ">
-                                    <span class="fa fa-4x fa-play"></span>
-                                </div>
-                            </div>
-                            <div class="video">
-                                <span class="video-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-                                <div class="youtube" data-embed="m2OEg1wI4QM">
-                                    <span class="fa fa-4x fa-play"></span>
-                                </div>
-                            </div>
-                            <div class="video">
-                                <span class="video-title">Lorem Ipsum</span>
-                                <div class="youtube" data-embed="civcLRSmpVQ">
-                                    <span class="fa fa-4x fa-play"></span>
-                                </div>
-                            </div>
-                            <div class="video">
-                                <span class="video-title">Lorem Ipsum</span>
-                                <div class="youtube" data-embed="3XKajzVTztg">
-                                    <span class="fa fa-4x fa-play"></span>
-                                </div>
-                            </div>
-                            <div class="video">
-                                <span class="video-title">Lorem Ipsum</span>
-                                <div class="youtube" data-embed="eQV4BVzPAxY">
-                                    <span class="fa fa-4x fa-play"></span>
-                                </div>
-                            </div>
-                            <div class="video">
-                                <span class="video-title">Lorem Ipsum</span>
-                                <div class="youtube" data-embed="USfeWUtLEdg">
-                                    <span class="fa fa-4x fa-play"></span>
-                                </div>
-                            </div>
-                            <div class="video">
-                                <span class="video-title">Lorem Ipsum</span>
-                                <div class="youtube" data-embed="5Gcnem-Elk0">
-                                    <span class="fa fa-4x fa-play"></span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col col-2"></div>
-                    </div>
+          						<div class="col col-2"></div>
+          						<div class="col col-8 video-gallery">
+          							<div class="video">
+          								<span class="video-title">Majestic - Boberek # Woodstock 2016 Live</span>
+          								<div class="youtube" data-embed="pFoSLLoRYMQ">
+          									<span class="fa fa-4x fa-play"></span>
+          								</div>
+          							</div>
+          							<div class="video">
+          								<span class="video-title">Majestic - Nie oglądaj się # Woodstock 2016 Live</span>
+          								<div class="youtube" data-embed="m2OEg1wI4QM">
+          									<span class="fa fa-4x fa-play"></span>
+          								</div>
+          							</div>
+          							<div class="video">
+          								<span class="video-title">Majestic - Wielka moc # Woodstock 216 Live</span>
+          								<div class="youtube" data-embed="civcLRSmpVQ">
+          									<span class="fa fa-4x fa-play"></span>
+          								</div>
+          							</div>
+          							<div class="video">
+          								<span class="video-title">Majestic - Połączmy nasze serca # Woodstock 2016 Live</span>
+          								<div class="youtube" data-embed="3XKajzVTztg">
+          									<span class="fa fa-4x fa-play"></span>
+          								</div>
+          							</div>
+          							<div class="video">
+          								<span class="video-title">Majetsic - SŁOWA, SŁOWA</span>
+          								<div class="youtube" data-embed="eQV4BVzPAxY">
+          									<span class="fa fa-4x fa-play"></span>
+          								</div>
+          							</div>
+          							<div class="video">
+          								<span class="video-title">Majestic MI-ŁO-Ś-Ć</span>
+          								<div class="youtube" data-embed="USfeWUtLEdg">
+          									<span class="fa fa-4x fa-play"></span>
+          								</div>
+          							</div>
+          							<div class="video">
+          								<span class="video-title">Majestic - TATKO</span>
+          								<div class="youtube" data-embed="5Gcnem-Elk0">
+          									<span class="fa fa-4x fa-play"></span>
+          								</div>
+          							</div>
+          						</div>
+          						<div class="col col-2"></div>
+          					</div>
                 </div>
             </section>
             <div class="row row-parallax" style="background-image: url('img/img-4.jpg');"></div>
@@ -346,19 +345,37 @@ mysqli_query($conn, 'SET NAMES `utf8` COLLATE `utf8_general_ci`');
                     <div class="row">
                         <div class="col col-4"></div>
                         <div class="col col-4 contact">
+                          <?php
+                          $sql = 'SELECT * FROM tbl_contact';
+                          $result = $conn->query($sql);
+                          if ($result->num_rows > 0) {
+                              while ($row = $result->fetch_assoc()) {
+                                  ?>
                             <address class="">
-                              <p><span class="fa fa-user fa-fw"></span><span>Loremiusz Ipsumski</span><br>
-                                  <a href="#"><span class="fa fa-envelope fa-fw"></span>loremiusz@ipsum.pl</a><br>
-                                  <a href="tel:+48-123-456-789"><span class="fa fa-phone fa-fw"></span>+48 123 456 789</a>
-                              </p>
-                              <p><span class="fa fa-user fa-fw"></span><span>Lorek Ipsum</span><br>
-                                  <a href="#"><span class="fa fa-envelope fa-fw"></span>loremipsumdolor@sitamet.pl</a><br>
-                                  <a href="tel:+48-666-666-666"><span class="fa fa-phone fa-fw"></span>+48 666 666 666</a>
+                              <p><span class="fa fa-user fa-fw"></span><span><?php echo $row['person']; ?></span><br>
+                                  <a href="#"><span class="fa fa-envelope fa-fw"></span><?php echo $row['email']; ?></a><br>
+                                  <a href="tel:+48-123-456-789"><span class="fa fa-phone fa-fw"></span><?php echo $row['phone']; ?></a>
                               </p>
                             </address>
+                            <?php }}?>
                             <p><span class="fa fa-file fa-fw"></span><span>Dokumenty</span><br>
-                                <a href="#"><span class="fa fa-download fa-fw"></span>Press Pack</a><br>
-                                <a href="#"><span class="fa fa-download fa-fw"></span>Rider</a>
+                              <?php
+                              $sql = 'SELECT * FROM tbl_files WHERE filetype = "presspack"';
+                              $result = $conn->query($sql);
+                              if ($result->num_rows > 0) {
+                                  while ($row = $result->fetch_assoc()) {
+                                      ?>
+                                <a href="files/<?php echo $row['filename']; ?>"><span class="fa fa-download fa-fw"></span>Press Pack</a><br>
+                              <?php }}?>
+                              <?php
+                              $sql = 'SELECT * FROM tbl_files WHERE filetype = "rider"';
+                              $result = $conn->query($sql);
+                              if ($result->num_rows > 0) {
+                                  while ($row = $result->fetch_assoc()) {
+                                      ?>
+                                <a href="files/<?php echo $row['filename']; ?>"><span class="fa fa-download fa-fw"></span>Rider</a><br>
+                              <?php }}?>
+
                             </p>
                         </div>
                         <div class="col col-4"></div>
@@ -372,6 +389,7 @@ mysqli_query($conn, 'SET NAMES `utf8` COLLATE `utf8_general_ci`');
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/velocity/1.4.1/velocity.min.js" charset="utf-8"></script>
     <script src="js/script.js" charset="utf-8"></script>
+    <?php $conn->close(); ?>
 </body>
 
 </html>
